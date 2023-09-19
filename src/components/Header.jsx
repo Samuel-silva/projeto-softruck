637c735bb44476efea9601b275cf645bcc87db16
Samuel Barbosa <samuca192silva@hotmail.com>
2023-09-19 09:41:53 -0300

    feat - Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import './Header.scss';
+import logo from '../image/logo.svg';
+
 function Header() {
   return (
     <header className="header">
       <div className="container-fluid">
-        <h1 className="header__title">Title</h1>
+        <div className="logo d-flex py-3 align-items-center justify-content-start">
+          <div className='logo__container d-flex'>
+            <img src={logo} alt="Logo Car Tracking" className='logo__img' />
+          </div>
+          <h1 className="logo__title ps-2 h5 text-white mb-0"><b>Car Tracking</b></h1>
+        </div>
       </div>
     </header>
   )
